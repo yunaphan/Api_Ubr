12408a46f76d060575c66f40865eb0951483d8c3
phan thi thuy tien <ptttien97@gmail.com>
2019-05-28 17:08:18 +0700

    new api router

---
+++ API/C_API/api_getListRoute.py
@@ -0,0 +1,11 @@
+from django.http import request
+from  django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.db import connections
+
+def getListRoute(request):
+    refcur = connections["DoThi"].cursor()
+    refcur.execute("Select MaDuong, TenDuong from sde.TIMDUONG")
+    row = dict()
+    row = refcur.fetchall()
+    return JsonResponse(row, safe=False)
--- API/urls.py
+++ API/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .C_API import api_GetSpecies, api_getStatus
+from .C_API import api_GetSpecies, api_getStatus, api_getListRoute
 # from . import views
 
 urlpatterns = [
-    path('getListSpecies/', api_GetSpecies.getTenCX, name="Lấy Tên Cây Xanh"),
-    path('getListStatus/', api_getStatus.getListStatusTree, name="Lấy Trạng Thái Cây xanh")
+    path('danh-sach-ten-cay-xanh/', api_GetSpecies.getTenCX, name="Ds Tên Cây Xanh"),
+    path('danh-sach-trang-thai-cay-xanh/', api_getStatus.getListStatusTree, name="Ds Trạng Thái Cây xanh"),
+    path('danh-sach-tuyen-duong/', api_getListRoute.getListRoute, name="Ds tuyến đường")
+
 ]
